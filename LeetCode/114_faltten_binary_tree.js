cfe86d8b3c31c74ea20c5870a7feb973c20f517c
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-11-01 10:43:05 -0400

    flatten binary tree to linked list

---
+++ LeetCode/114_faltten_binary_tree.js
@@ -0,0 +1,43 @@
+// Given a binary tree, flatten it to a linked list in-place.
+//
+// For example, given the following tree:
+//
+//     1
+//    / \
+//   2   5
+//  / \   \
+// 3   4   6
+// The flattened tree should look like:
+//
+// 1
+//  \
+//   2
+//    \
+//     3
+//      \
+//       4
+//        \
+//         5
+//          \
+//           6
+
+// Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+
+function flatten_tree(node) {
+  let prev = null;
+  let dfs = function(node) {
+  if (!node) return null;
+  dfs(node.right);
+  dfs(node.left);
+
+  node.right = prev;
+  node.left = null;
+  prev = node;
+  }
+  dfs(root)
+
+}
