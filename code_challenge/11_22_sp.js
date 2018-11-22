dedf2480c1ce046ea36ea11b2ac7fa986ca3452a
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-11-21 20:18:37 -0500

    code challenge 11_22 answered

---
+++ code_challenge/11_22_sp.js
@@ -0,0 +1,27 @@
+//site corp
+What tag do you use to implement the google API?
+script tag
+What code used to create the animation on the homepage?
+@keyframes Animate{
+  0%{background-image: url("https://cdngeneral.rentcafe.com/dmslivecafe/3/162865/_PRP0840.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&scale=both&");}
+  33%{background-image: url("https://media2.s-nbcnews.com/i/newscms/2016_29/1144622/home-vacation-prep-today-160720-tease_b4ae657a1e09780845d75b604be99522.jpg");}
+  66% {background-image: url("https://img.diytrade.com/cdimg/1683186/24304595/0/1324632494/Background_Wall_Glass.jpg");}
+}
+
+.homepage {
+  background-image: url("https://cdngeneral.rentcafe.com/dmslivecafe/3/162865/_PRP0840.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&scale=both&");
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  box-sizing: border-box;
+  height: 900px;
+  animation: Animate 18s infinite;
+}
+
+What is the difference between 'var' 'let' and 'constant'
+'var' function scope, valid in entire function
+'let' block scope,
+'const' block scope, A variable created with const cannot be changed after it is initialized.
+
+How do you create the app?
+How dose the google map work? the pointer ?
