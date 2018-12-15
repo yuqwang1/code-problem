358ecc67f8776b9913a34e44191429e862f7bc14
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-12-14 22:18:39 -0500

    finish the record of interview question

--- code_challenge/12_13_HHMayes.js
+++ code_challenge/12_13_HHMayes.js
@@ -1,8 +1,22 @@
 SQL question?
 1.Left join, right join?
 2.Foreign key, what is the feature to use foreign key?
+3.Data Manipulation language?
+4.SQL Data Type?
+5.SQL storage procedure?
+6.What is view?
 
 JavaScript Question?
 1. variable scope?
 Global scope, function scope
-2.
+2.What is preventDefault?
+3.What is stoppropgation?
+4.What are some false value in JavaScript?
+5.Use jQuery to write "when user click a button, a message shows up"
+6.How do you debug JavaScipt errors for frontend ?
+7.What do you know about event bubbling?
+8.
+
+CSS
+1. What is Position ?
+2. What is float ?
