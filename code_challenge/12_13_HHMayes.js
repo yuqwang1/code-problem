4fe772750285417a9bede8b4dc13effbf50d31af
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-12-14 22:15:36 -0500

    add HHMayes interview question

---
+++ code_challenge/12_13_HHMayes.js
@@ -0,0 +1,8 @@
+SQL question?
+1.Left join, right join?
+2.Foreign key, what is the feature to use foreign key?
+
+JavaScript Question?
+1. variable scope?
+Global scope, function scope
+2.
--- code_challenge/12_14_JPMC.js
+++ code_challenge/12_14_JPMC.js
@@ -1,4 +1,8 @@
 How React works
 What is the limitation of React
 When click on the component, what happens?
-How do you connect the frontend with the backend 
+How do you connect the frontend with the backend ?
+What do you know about the redux? The store ?
+What is the life cycle of component? Which is the essential lifecycle for every component?
+render method.
+How is the render method works?
