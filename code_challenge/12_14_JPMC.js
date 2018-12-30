07012bdbc8a23f8836450b9dde376f3252c17462
Yuqian Wang <iamwangyuqian@yahoo.com>
2018-12-29 23:10:32 -0500

    keep finish interview question

--- code_challenge/12_14_JPMC.js
+++ code_challenge/12_14_JPMC.js
@@ -2,7 +2,9 @@ How React works
 What is the limitation of React
 When click on the component, what happens?
 How do you connect the frontend with the backend ?
+need the whole data flow process.
 What do you know about the redux? The store ?
+redux store all the state.
 What is the life cycle of component? Which is the essential lifecycle for every component?
 render method.
 How is the render method works?
