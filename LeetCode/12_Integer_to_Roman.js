3fac5f4765d3319cdbf2ef47ebe0f2af68082e30
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-01-19 23:32:32 -0500

    complete question 12

--- LeetCode/12_Integer_to_Roman.js
+++ LeetCode/12_Integer_to_Roman.js
@@ -56,21 +56,34 @@ function Integer_to_Roman(number){
     if (number >= 1000){
       result += 'M';
       number -= 1000;
+    } else if (number >= 900) {
+      result += 'CM';
+      number -= 900;
     } else if (number >= 500 ) {
       result += 'D';
       number -= 500;
     } else if (number >= 100) {
       result += 'C';
       number -= 100;
+    } else if (number > 90) {
+      result += 'XC';
+      number -= 90;
     } else if (number >= 50) {
       result += 'L';
       number -= 50;
     } else if (number >= 10) {
       result += 'X';
       number -= 10;
-    } else if (number >= 5) {
+    } else if (number >= 9) {
+      result += 'IX';
+      number -= 9;
+    }
+      else if (number >= 5) {
       result += 'V';
       number -= 5;
+    } else if (number >= 4) {
+      result += 'IV';
+      number -= 4;
     } else if (number >= 1) {
       result += 'I';
       number -= 1;
