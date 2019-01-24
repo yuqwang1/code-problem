ecc5e3f8e32b97d7373e951a129c3b37d573baa1
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-01-23 21:56:10 -0500

    first attempt question 14

--- LeetCode/14_longest_prefix_match.js
+++ LeetCode/14_longest_prefix_match.js
@@ -21,3 +21,17 @@ Example 2:
 Input: ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
+
+var longestCommonPrefix = function(strs) {
+  let n = strs.length;
+  let result = '';
+
+  for (let i = 0; i < n; i++) {
+    if (strs[i][j] == strs[i + 1][j]){
+      continue;
+    } else {
+      return result;
+    }
+    j++;
+  }
+};
