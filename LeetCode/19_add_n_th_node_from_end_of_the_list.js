9bd91c55b833fca2aaca3b7807f90a6e0c34f877
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-02-02 23:15:30 -0500

    add question 19

---
+++ LeetCode/19_add_n_th_node_from_end_of_the_list.js
@@ -0,0 +1,17 @@
+19. Remove Nth Node From End of List
+Medium
+
+1459
+
+114
+
+Favorite
+
+Share
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
