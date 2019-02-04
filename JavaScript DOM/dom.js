573bb719aff9fdb478bf0f3b689aaf64be51a5ed
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-02-04 18:26:01 -0500

    JS DOM crash

---
+++ JavaScript DOM/dom.js
@@ -0,0 +1,17 @@
+// Examine the document object
+
+// console.dir(document);
+//
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+//
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
