63b1ef9dc1d9c7bd3b0c9f4a9755201ffa235281
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-02-07 20:00:06 -0500

    miles_technologies video interview

---
+++ code_challenge/02_07_mile_technologies.js
@@ -0,0 +1,6 @@
+1. Describe how would you design a software for recruiting company ?
+2. What data structure would you construct ?
+3. Tell me how happy you are ? Range 1 - 10
+4. Tell me about any project you proud of ?
+5. UI vs UX ?
+6. How is a component works ? 
