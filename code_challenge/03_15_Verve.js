c7982e5aed7b93ab7c45b519d643324c498bb355
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-03-14 15:31:41 -0400

    add verve technical interview

---
+++ code_challenge/03_15_Verve.js
@@ -0,0 +1,10 @@
+1. what is 'hoisting'?
+2. what is differece between let, var, const
+3. Event bubbling, two div outer div and inner div
+click on the inner div, will the outer fire the info
+yes
+4.how to prevent outer div from fire the info
+stopPropagation
+5.how to prevent inner div from fire the info
+6.difference between inline block and inline ?
+ 
