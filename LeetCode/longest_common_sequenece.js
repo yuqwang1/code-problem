bd318de5f5dae9cb8903e39a353d362b0e42328e
Yuqian Wang <iamwangyuqian@yahoo.com>
2019-08-10 11:19:23 +0800

    update 8-10

---
+++ LeetCode/longest_common_sequenece.js
@@ -0,0 +1,17 @@
+1143. Longest Common Subsequence
+Medium
+
+92
+
+6
+
+Favorite
+
+Share
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+
+
+If there is no common subsequence, return 0.
